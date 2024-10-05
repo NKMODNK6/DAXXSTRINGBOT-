d6031744169e622bf2ba52067c74fb3839682ade
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-05 21:38:02 +0530

    Update __init__.py

--- DAXXSTRINGBOT/db/__init__.py
+++ DAXXSTRINGBOT/db/__init__.py
@@ -1,7 +1,6 @@
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
-
+from motor.motor_asyncio import AsyncIOMotorClient
 import config
 
+_mongo_async_ = AsyncIOMotorClient(config.MONGO_DB_URI)
+db = _mongo_async_.StringGen
 
-mongo = MongoCli(config.MONGO_DB_URI)
-db = mongo.StringGen
