64314f4678ebf16d3c52ff4420f3c5e6da82fbaf
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-10-05 21:38:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "24509589"))
 API_HASH = getenv("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75")
-
 BOT_TOKEN = getenv("BOT_TOKEN", "7493562717:AAH9DGdNRk5g_kCQ6hxLR39szY-WacI8At0")
 OWNER_ID = int(getenv("OWNER_ID", "7427691214"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "gitwizardbypass")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://MehraMusicbot:MehraMusicbot@mehramusicbot.idpksvs.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("MUST_JOIN", "Resso_music_group")
 DAXX_API = getenv("DAXX_API", "daxx-1490?api+key:free")
